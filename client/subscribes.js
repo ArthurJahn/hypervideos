d8287519650e24c623d2329ba4d91d10e2b3e5fa
ArthurJahn <stutrzbecher@gmail.com>
2015-09-29 01:15:54 -0300

    Refactoring panels and routing templates and data

--- client/subscribes.js
+++
@@ -1,5 +0,0 @@
-Meteor.subscribe("hypervideos");
-Meteor.subscribe("videos");
-Meteor.subscribe("questions");
-Meteor.subscribe("subjects");
-Meteor.subscribe("subvideos");
--- client/view/templates/main-menu/main-menu.js
+++ client/view/templates/main-menu/main-menu.js
@@ -1,29 +1,20 @@
-//Used in title template
-Session.setDefault('title', 'Novo Curso');
-//Used in panel logic
-Session.setDefault('activePanel', '3');
-
-Template.mainMenu.helpers({
-  activePanel: function() {
-    return Session.get('activePanel');
-  }
+Accounts.ui.config({
+  passwordSignupFields: "USERNAME_ONLY"
 });
+//Used in title template
+Session.setDefault('title', 'Explorar');
 
 Template.mainMenu.events({
   'click #subjects': function () {
     Session.set('title', 'Meus Cursos');
-    Session.set('activePanel', '0');
   },
   'click #profile': function () {
     Session.set('title', 'Perfil');
-    Session.set('activePanel', '1');
   },
   'click #explore': function () {
     Session.set('title', 'Explorar');
-    Session.set('activePanel', '2');
   },
   'click #highlighted': function () {
     Session.set('title', 'Novo Curso');
-    Session.set('activePanel', '3');
   }
 });
