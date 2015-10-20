20e98248a71abdb3d75170976b189b7178371288
ArthurJahn <stutrzbecher@gmail.com>
2015-10-19 22:14:43 -0200

    #25 - adding linked subvideos when watching a video

--- client/template/watch-subject/watch-subject.js
+++ client/template/watch-subject/watch-subject.js
@@ -12,7 +12,14 @@ Template.watchSubject.events({
   'get-hypervideos subject-player': function (e, template) {
     var subject = Subject.findOne({_id : e.target.subject._id});
     e.target.hypervideos = subject.hypervideos();
-    console.log(subject.hypervideos());
   },
-
+  'get-subvideos hyper-player': function (e, template) {
+    var hypervideo = Hypervideo.findOne({_id : e.target.hypervideo._id});
+    console.log(hypervideo);
+    e.target.subvideos = hypervideo.subvideos();
+  },
+  'get-questions hyper-player': function (e, template) {
+    var hypervideo = Hypervideo.findOne({_id : e.target.hypervideo._id});
+    e.target.questions = hypervideo.questions();
+  },
 });
