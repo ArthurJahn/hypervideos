9e6c1578f4c6283a74263c4aa956948640a867c1
ArthurJahn <stutrzbecher@gmail.com>
2015-10-30 22:56:27 -0200

    #56 and #57 - Methods for adding subvideos and questions to visited hypervideo

--- client/model/VisitedHypervideo.js
+++ client/model/VisitedHypervideo.js
@@ -2,30 +2,69 @@ VisitedHypervideo = Astro.Class({
   name: 'VisitedHypervideo',
   collection: VisitedHypervideos,
   fields: {
+
+    // id that interconnects this visited hypervideo
+    // with its subject and user that is watching
+    // the subject
     librarySubjectId: {
       type: 'string',
       validator: Validators.required(),
     },
+
+    // hypervideo that has been visited
+    // by the user. This represents from witch
+    // hypervideo subvideos and questions will
+    // be collected to analise subject's completion
     hypervideoId: {
       type: 'string',
       validator: Validators.required(),
     },
+
+    // if all subvideos were watched,
+    // this hypervideo is set as completed
     completed: {
       type: 'boolean',
       default: false
     },
+
+    // user watched subvideos collected from
+    // specified hypervideo
     subvideos: {
       type: 'array',
       default: function() {
         return [];
       }
     },
+
+    // user answered questions collected from
+    // specified hypervideo
     questions: {
       type: 'array',
       default: function() {
         return [];
       }
     }
+  },
+  methods: {
+
+    addWatchedSubvideo: function(subvideoId) {
+      if(this.subvideos.indexOf(subvideoId)!== -1) {
+        this.push('subvideos', subvideoId);
+        return true;
+      }
+      return false;
+    },
+
+    addAnsweredQuestion: function(questionAnswer) {
+      if(questionAnswer.questionId && questionAnswer.answer) {
+        if(this.questions.indexOf(questionAnswer)!== -1){
+          this.push('questions', questionAnswer);
+          return true;
+        }
+      }
+      return false;
+    },
+
   },
   behaviors: ['timestamp']
 });
