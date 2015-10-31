568a27d71bed8ecd667a700adf1a0fcb555664e7
ArthurJahn <stutrzbecher@gmail.com>
2015-10-30 23:55:43 -0200

    #49 - creating connection between user, subject and visited hypervideo

--- client/template/watch-subject/watch-subject.js
+++ client/template/watch-subject/watch-subject.js
@@ -11,7 +11,14 @@ Template.watchSubject.helpers({
 Template.watchSubject.events({
   'get-hypervideos subject-player': function (e, template) {
     var subject = Subject.findOne({_id : e.target.subject._id});
-    e.target.hypervideos = subject.hypervideos();
+    var hypervideos = [];
+    subject.hypervideos().forEach(function(hypervideo){
+      hypervideos.push(hypervideo.get());
+    });
+    e.target.hypervideos = hypervideos;
+    //FIX_ME: calculate hypervideo to be seen at first
+    e.target.hypervideo = hypervideos[0];
+    Template.watchSubject.visitHypervideo(hypervideos[0]._id);
   },
   'get-subvideos hyper-player': function (e, template) {
     var hypervideo = Hypervideo.findOne({_id : e.target.hypervideo._id});
@@ -23,12 +30,24 @@ Template.watchSubject.events({
   },
   'get-source-subvideo hyper-player': function (e, template) {
     var hypervideo = Hypervideo.findOne({_id : e.target.hypervideo._id});
+    //FIX-ME: calculate next subvideo to be played
     var subvideo = hypervideo.subvideos()[0];
     var sourceSubvideo = subvideo.get();
     sourceSubvideo.source = subvideo.media().url();
     e.target.subvideo = sourceSubvideo;
   },
   'hypervideo-visited hyper-player': function(e, template) {
+    
     var hypervideo = Hypervideo.findOne({_id : e.target.hypervideo._id});
+
   },
 });
+
+Template.watchSubject.visitHypervideo = function(hypervideoId){
+  var libSubject = LibrarySubject.findOne();
+  var visitHypervideo = new VisitedHypervideo({
+    librarySubjectId: libSubject._id,
+    hypervideoId: hypervideoId
+  });
+  visitHypervideo.save();
+};
